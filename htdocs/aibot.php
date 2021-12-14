039330648b95c03a19e3b9a4849dc3cd0a4baae8
Stefano Fancello <stefano.fancello@nethesis.it>
2021-12-14 18:16:36 +0100

    Add AI bot

---
+++ htdocs/aibot.php
@@ -0,0 +1,92 @@
+<?php
+
+/*
+ * This is part of Api Call FreePBX module. Make call using Rest API
+ * Copyright (C) 2021  Stefano Fancello gentoo.stefano@gmail.com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+include '/etc/freepbx_db.conf';
+$sql = 'SELECT `val` FROM `kvstore_FreePBX_modules_Apicall` WHERE `key` = "token"';
+$sth = $db->prepare($sql);
+$sth->execute();
+$token = $sth->fetchAll()[0][0];
+
+if (empty($_SERVER['HTTP_TOKEN']) || $_SERVER['HTTP_TOKEN'] !== $token) {
+	http_response_code(401);
+	exit(1);
+}
+
+include '/etc/freepbx.conf';
+session_start();
+define('FREEPBX_IS_AUTH', 1);
+// Include all installed modules class
+$post = json_decode(file_get_contents('php://input'), true);
+$tocall = $post['PhoneNumber'];
+$maxretries = !empty($post['maxretries']) ? $post['maxretires'] : 0;
+$retrytime = !empty($post['retrytime']) ? $post['retrytime'] : 60;
+$waittime = !empty($post['waittime']) ? $post['waittime'] : 30;
+$callerid = !empty($post['callerid']) ? $post['callerid'] : '999';
+
+if (empty($tocall)) {
+	error_log('ApiCall: missing tocall');
+	http_response_code(400);
+	exit(1);
+}
+
+$input_parameters = array(
+        'ContactId' => null,
+        'CampaignId' => null,
+        'MessageId' => null,
+        'PhoneNumber' => null,
+        'MessageUrl' => null,
+        'MessageText' => null,
+        'UserInputMethod' => false, // false | 'voice' | 'digits'
+        'MessageExitDigit' => '', // ''|123456789*# stop playing message and exit if one of digits is pressed
+        'NuberOfExpectedDigits' => 0,
+        'EndDigit' => null,
+        'UserAnswerSTT' => null,
+        'CallStatusWebhookUrl' => null,
+        'CallStatusWebhookHeaders' => '',
+        'GoToDestination' => null, // If setted, go to this destination instead of process next POST
+        'NextMessageWebhookUrl' => null,
+        'NextMessageWebhookHeaders' => '',
+);
+
+$content = "Channel: Local/{$tocall}@from-internal
+MaxRetries: {$maxretries}
+RetryTime: {$retrytime}
+WaitTime: {$waittime}
+Callerid: {$callerid}
+Context: aibot
+Priority: 1
+Extension: s
+";
+
+foreach ( $input_parameters as $p => $default_value) {
+	$value = isset($post[$p]) ? $post[$p] : $default_value;
+	// TODO escape $value
+
+	$content .= "Setvar: $p=".base64_encode($value)."\n";
+}
+
+$tmp_name = tempnam("/tmp", 'aibot_call');
+$f = fopen($tmp_name,"w");
+fwrite($f,$content);
+fclose($f);
+$filename = "aibot_call_".microtime(true).".call";
+$res = rename($tmp_name,"/var/spool/asterisk/outgoing/$filename");
+
